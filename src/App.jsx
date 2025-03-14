93bbc780a802ce078a657a39a1b4669cbfac2c84
Johan Villanueva <jvillanuevagallego@gmail.com>
2025-03-13 21:50:36 -0500

    Some changes5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import { Networks } from "./Layouts/Networks/Networks"
-import { Header } from "./Layouts/Header/Header"
-import { AboutMe } from "./Layouts/About_me/AboutMe"
-import { Technologies } from "./Layouts/Technologies/Technologies"
-import { SocialSkills } from "./Layouts/SocialSkills/SocialSkills"
-import { Experience } from "./Layouts/Experience/Experience"
-import { Certificatess } from "./Layouts/Certificates/Certificatess"
+import  Networks  from "./Layouts/Networks/Networks"
+import  Header  from "./Layouts/Header/Header"
+import  AboutMe  from "./Layouts/About_me/AboutMe"
+import  Technologies  from "./Layouts/Technologies/Technologies"
+import  SocialSkills  from "./Layouts/SocialSkills/SocialSkills"
+import  Experience  from "./Layouts/Experience/Experience"
+import  Certificatess  from "./Layouts/Certificates/Certificatess"
 
 export const App = () => {
   return (
@@ -14,10 +14,10 @@ export const App = () => {
       <Networks />
       <Routes>
         <Route path="/Portfolio" element={<AboutMe />} />
-        <Route path="/Portfolio/Technologies" element={<Technologies />} />
-        <Route path="/Portfolio/SocialSkills" element={<SocialSkills />} />
-        <Route path="/Portfolio/Experience" element={<Experience />} />
-        <Route path="/Portfolio/Certificates" element={<Certificatess />} />
+        <Route path="/Portfolio/technologies" element={<Technologies />} />
+        <Route path="/Portfolio/social-skills" element={<SocialSkills />} />
+        <Route path="/Portfolio/experience" element={<Experience />} />
+        <Route path="/Portfolio/certificates" element={<Certificatess />} />
       </Routes>
     </>
   )
--- src/UI/Certificates/Certifi.jsx
+++ src/UI/Certificates/Certifi.jsx
@@ -1,12 +1,10 @@
-import { Link } from 'react-router-dom';
-
 export const Certifi = ({ route, children, titleCert }) => {
     return (
         <>
-                <Link to={route} id="cert" className="bg-custom-950 w-[300px] h-[130px] flex flex-col text-center text-custom-300 items-center justify-center">
+                <a href={route} id="cert" target='_blank' className="bg-custom-950 w-[300px] h-[130px] flex flex-col text-center text-custom-300 items-center justify-center">
                     <h4>{titleCert}</h4>
                     <p>{children}</p>
-                </Link>
+                </a>
         </>
     )
 }
